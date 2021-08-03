78a4092adcd7eb2d2ce6a990f624e5e6219c6097
Benjamin "Ewosoft" Efenudu <84343391+benjaminefenudu@users.noreply.github.com>
2021-08-03 21:50:46 +0100

    Update userUpdate.js

--- src/controllers/userUpdate.js
+++ src/controllers/userUpdate.js
@@ -18,8 +18,10 @@ const updateInfo = async (req, res) => {
   try {
     let user = await User.findById(req.user.id);
 
+    // Limit user info changes via this route to only the following
     const { firstName, lastName, phoneNo, businessName } = req.body;
-
+    
+    // User can update or leave out any of these fields
     if (firstName) user.firstName = firstName;
     if (lastName) user.lastName = lastName;
     if (phoneNo) user.phoneNo = phoneNo;
